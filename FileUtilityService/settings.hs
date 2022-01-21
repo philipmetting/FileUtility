bd3b16b1a4fa0c35bf52b9074c85ea5a833b04f6
Philip Metting <philip.metting@gmail.com>
2022-01-21 14:53:43 -0600

    updating settings

--- FileUtilityService/settings.hs
+++ FileUtilityService/settings.hs
@@ -2,8 +2,8 @@
 OutputFolderLocation|c:\Support\
 CopyFolderLocation|C:\Fax Backup\
 DaysToKeepCopy|30
-UsersToEmail|philipmetting@gmail.com
+UsersToEmail|email@domain.com
 FromEmail|Fax@gmail.com
 EmailSubject|Incoming Fax
 EmailBody|You have received a fax!
-SupportEmail|philipmetting@gmail.com
+SupportEmail|email@domain.com
